e900e774542ea506b77b3a318348017fc5834e17
hoanghai1310 <hoanghai.131096@gmail.com>
2022-06-02 14:06:35 +0700

    upp

---
+++ src/sort/SortByScore.java
@@ -0,0 +1,15 @@
+package sort;
+
+import Models.Student;
+
+import java.util.Comparator;
+
+public class SortByScore implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.getDiemTB() > o2.getDiemTB()) {
+            return 1;
+        }
+        return -1;
+    }
+}
